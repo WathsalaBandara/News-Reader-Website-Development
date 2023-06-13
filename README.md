c78418bb7bd20e55fa9df904c24f5b7f3c2e805c
Wathsala Bandara <60806985+WathsalaBandara@users.noreply.github.com>
2023-06-13 15:23:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# News-Reader-Website-Development
-Developed a news reader website that aggregates news articles from various sources and allows users to read them in one place The website was developed using HTML, CSS, JavaScript, and PHP
+# News-Site
+ CMIS 3224 - Group Project 
---
+++ about-us.php
@@ -0,0 +1,81 @@
+<?php
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>News Portal | About us</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <?php include('includes/header.php');?>
+    <!-- Page Content -->
+    <div class="container">
+
+<?php 
+$pagetype='aboutus';
+$query=mysqli_query($con,"select PageTitle,Description from tblpages where PageName='$pagetype'");
+while($row=mysqli_fetch_array($query))
+{
+
+?>
+      <h1 class="mt-4 mb-3"><?php echo htmlentities($row['PageTitle'])?>
+  
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">About</li>
+      </ol>
+
+      <div class="row">
+        <div class="col-lg-12">
+          
+          <h2><span style="font-weight: 600;">Our Mission</span></h2><h5><span style="font-weight: 600;">&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</span>Our mission is to provide free simple php projects to our user. These projects are only for educational purpose.</h5><h5>User can easily modify source code and adjust it for their purpose.&nbsp;</h5>
+          <p>
+<br>
+<br><br><br><br><br>
+
+
+
+</p>
+          </div>
+      </div>
+
+      <!-- Intro Content 
+      <div class="row">
+
+        <div class="col-lg-12">
+
+          <p><?php echo $row['Description'];?></p>
+        </div>
+      </div>
+       /.row -->
+<?php } ?>
+    
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+ <?php include('includes/footer.php');?>
+
+ 
